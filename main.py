0f949421ed528eecdc4df2c45e0f7ec381098915
kubo2002 <gubany1@stud.uniza.sk>
2025-06-02 23:01:06 +0200

    output data path has been changed

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from scrapper.RawData import RawData
 if __name__ == '__main__':
     scrapper = RawData("https://www.nehnutelnosti.sk/vysledky")
     scrapper.scrape()
-    scrapper.save_to_csv("byty_martin_predaj.csv")
+    scrapper.save_to_csv("data/raw_data.csv")
 
